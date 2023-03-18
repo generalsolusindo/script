f57f4174f7686b435b3edd7857ae6a74f7a467a9
General Solusindo <104060104+generalsolusindo@users.noreply.github.com>
2023-03-19 03:08:15 +0700

    Create agent.sh

---
+++ agent.sh
@@ -0,0 +1,5 @@
+# Wazuh
+curl -so wazuh-agent-4.3.10.deb https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.3.10-1_amd64.deb && sudo WAZUH_MANAGER='10.10.10.45' WAZUH_AGENT_GROUP='default' dpkg -i ./wazuh-agent-4.3.10.deb && sudo systemctl daemon-reload && sudo systemctl enable wazuh-agent && sudo systemctl start wazuh-agent
+
+# SNMP
+sudo apt install snmpd snmp libsnmp-dev && sudo cp /etc/snmp/snmpd.conf{,.bak} && sudo sed -i '/agentaddress  127.0.0.1,\[::1\]/a\agentaddress 10.10.10.44' /etc/snmp/snmpd.conf && sudo sed -i 's/^agentaddress  127.0.0.1,\[::1\]/#&/' /etc/snmp/snmpd.conf && sudo systemctl restart snmpd
